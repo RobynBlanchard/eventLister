13e13e2bfb809828d1e271f13d38ebe777db9f31
Robyn <Robyn@nothsadmins-MacBook-Pro.local>
2017-12-21 22:09:28 +0000

    correct mechanize script

--- app/mechanize.rb
+++ app/mechanize.rb
@@ -12,11 +12,8 @@ link_to_event = page.link_with(text: 'Events')
 
 page = link_to_event.click
 
-#link_to_UK_events = page.link_with(text: '&nbsp; UK')
-#link_to_UK_events = page.link_with(text: 'UK')
-next_page = page.at('#Form1 > main > ul > li > section > div > ul > li.fl.col2 > div:nth-child(4) > ul > li:nth-child(25) > div:nth-child(2) > a')
-#page = next_page.click
+link_to_UK_events = page.link_with(:href => '/dj/denissulta/dates?ctry=3')
 
-#page = link_to_UK_events.click
+page = link_to_UK_events.click
 
-puts next_page.methods
+puts page.uri
