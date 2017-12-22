27dd13c92b625e4af1a2a2a9e606b96800bd92e1
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2017-12-22 11:33:54 +0000

    only require artist name to get event listings

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,14 +1,17 @@
 class EventsController < ApplicationController
   def index
-    artists_and_urls = create_dictionary()
-    artist_events_and_location = {}
+    get_list_of_artists_and_events
+  end
+
+  def get_list_of_artists_and_events
+    artists_and_urls = create_dictionary_of_artists_and_urls
+    events_and_locations = {}
     artists_and_urls.each do |artist, url|
-      result = scrapeRA(url)
-      artist_events_and_location[artist] = result
-      @results = artist_events_and_location
+      event_and_location = scrapeRA(url)
+      events_and_locations[artist] = event_and_location
+      @results = events_and_locations
     end
     @results
-
   end
 
   def scrapeRA(url)
@@ -17,12 +20,12 @@ class EventsController < ApplicationController
     nokogiri_object = Nokogiri::HTML(html_data)
     event_elements = nokogiri_object.xpath("//ul[@class='list']/li/article/div/h1/a")
     event_elements_location = nokogiri_object.xpath("//ul[@class='list']/li/article/div/h1/span")
-    events = get_info(event_elements)
-    event_locations = get_info(event_elements_location)
-    artist_event_and_location = Hash[events.zip(event_locations)]
+    events = extract_text(event_elements)
+    event_locations = extract_text(event_elements_location)
+    artist_events_and_locations = Hash[events.zip(event_locations)]
   end
 
-  def get_info(elements)
+  def extract_text(elements)
     events = []
     elements.each do |event|
       events << event.text
@@ -30,7 +33,7 @@ class EventsController < ApplicationController
     events
   end
 
-  def create_dictionary()
+  def create_dictionary_of_artists_and_urls()
     @uploads = Upload.all
     artists_and_urls = {}
     @uploads.each do |upload|
@@ -38,21 +41,4 @@ class EventsController < ApplicationController
     end
     artists_and_urls
   end
-
-  def mechanise(artist_name)
-    require 'mechanize'
-    mechanize = Mechanize.new
-    page = mechanize.get('https://www.residentadvisor.net/dj.aspx')
-    link = page.link_with(text: artist_name)
-    page = link.click
-    link_to_event = page.link_with(text: 'Events')
-    page = link_to_event.click
-    artist = artist_name.downcase.delete(' ')
-    # TO DO: change this to click link with text UK
-    link_to_UK_events = page.link_with(:href => "/dj/#{artist}/dates?ctry=3")
-    page = link_to_UK_events.click
-    page.uri.to_s
-  end
-
-
 end
--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,6 +2,7 @@ class UploadsController < ApplicationController
   def index
     @uploads = Upload.all
   end
+
   def new
   end
 
@@ -10,8 +11,9 @@ class UploadsController < ApplicationController
   end
 
   def create
-    @upload = Upload.new(params.require(:upload).permit(:artist, :url))
-
+    artist = params[:artist]
+    url = mechanise(artist)
+    @upload = Upload.new(artist: artist, url: url)
     @upload.save
     redirect_to @upload
   end
@@ -21,5 +23,20 @@ class UploadsController < ApplicationController
     @upload.destroy
 
     redirect_to uploads_path
+  end
+
+  def mechanise(artist_name)
+    require 'mechanize'
+    mechanize = Mechanize.new
+    page = mechanize.get('https://www.residentadvisor.net/dj.aspx')
+    link = page.link_with(text: artist_name)
+    page = link.click
+    link_to_event = page.link_with(text: 'Events')
+    page = link_to_event.click
+    artist = artist_name.downcase.delete(' ')
+    # TO DO: change this to click link with text UK
+    link_to_UK_events = page.link_with(:href => "/dj/#{artist}/dates?ctry=3")
+    page = link_to_UK_events.click
+    page.uri.to_s
   end
 end
