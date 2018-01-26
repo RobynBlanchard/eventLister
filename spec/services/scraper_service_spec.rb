16baf705569b859941a6bf7f90bc99fb126f8ea0
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2018-01-26 13:21:01 +0000

    start using vcr gem

--- spec/services/scraper_service_spec.rb
+++ spec/services/scraper_service_spec.rb
@@ -11,6 +11,19 @@ describe ScraperService do
       end
 
       # check that it actually saves an event title, location etc?
+      it "does not throw an error" do
+        VCR.use_cassette('correct_result') do
+          artist = Artist.create(artist_name: 'Jackmaster', url: "https://www.residentadvisor.net/dj/jackmaster/dates?ctry=3")
+          scraper = ScraperService.new(artist)
+          res = scraper.scrapeRA
+          byebug
+          expect(res.keys).to include('data')
+          expect(res['data'].keys).to include('children')
+          expect(res['data']['children'].count).to be >= 1
+          expect(res['data']['children'][0].keys).to include('data')
+          # test more stuff!
+        end
+      end
     end
   end
 end
