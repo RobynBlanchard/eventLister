d99b939314b438454d848f128abc7b288e7ae9fe
Robyn <Robyn@nothsadmins-MacBook-Pro.local>
2018-02-04 22:18:59 +0000

    split scrapeRA method into separate methods

--- app/services/scraper_service.rb
+++ app/services/scraper_service.rb
@@ -16,21 +16,29 @@ class ScraperService
     dates_count = 0
     list_elements.each do |event|
       if count%2 == 0
-        event_date = event.text.split('/').first.rstrip
-        dates << event_date
+        dates << event_date(event)
       else
-        event_title = event.xpath("a").text
-        event_location = event.xpath("span").text
         #first_or_create not working, consider using find or create ?
-        
-        unless Event.exists?(artist_id: artist.id, location: event_location, event_title: event_title, event_date: dates[dates_count])
-          event = Event.create(artist_id: artist.id, location: event_location, event_title: event_title, event_date: dates[dates_count])
+        unless Event.exists?(artist_id: artist.id, location: event_location(event), event_title: event_title(event), event_date: dates[dates_count])
+          event = Event.create(artist_id: artist.id, location: event_location(event), event_title: event_title(event), event_date: dates[dates_count])
           event.save
         end
         dates_count += 1
       end
       count +=1
     end
+  end
+
+  def event_title(event)
+    event.xpath("a").text
+  end
+
+  def event_location(event)
+    event.xpath("span").text
+  end
+
+  def event_date(event)
+    event.text.split('/').first.rstrip
   end
 
   private
